cc7a270e6c6b041e8a2cafebc5eefcffd6da0fed
Chad Godsey <LyleBarbatuous@gmail.com>
2011-07-18 11:01:55 -0500

    adding windows bat files that do important things like copy files and run the executable with a test file

---
+++ prepare.bat
@@ -0,0 +1,8 @@
+XCOPY Data\*.* MSVC\Debug\Data /C /S /D /Y /I
+XCOPY Data\*.* MSVC\Release\Data /C /S /D /Y /I
+
+XCOPY Images\*.* MSVC\Debug\Images /C /S /D /Y /I
+XCOPY Images\*.* MSVC\Release\Images /C /S /D /Y /I
+
+XCOPY Sound\*.* MSVC\Debug\Sound /C /S /D /Y /I
+XCOPY Sound\*.* MSVC\Release\Sound /C /S /D /Y /I
---
+++ prepare_run.bat
@@ -0,0 +1,10 @@
+XCOPY Data\*.* MSVC\Debug\Data /C /S /D /Y /I
+XCOPY Data\*.* MSVC\Release\Data /C /S /D /Y /I
+
+XCOPY Images\*.* MSVC\Debug\Images /C /S /D /Y /I
+XCOPY Images\*.* MSVC\Release\Images /C /S /D /Y /I
+
+XCOPY Sound\*.* MSVC\Debug\Sound /C /S /D /Y /I
+XCOPY Sound\*.* MSVC\Release\Sound /C /S /D /Y /I
+
+MSVC\Release\Galaxy2D.exe -map level3
